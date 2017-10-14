dbeb449cc57ec9db6abce87b2b510f31da92add2
vasbr <you@example.com>
2017-10-14 20:11:06 +0300

    allages savvato brady

---
+++ Qualco3/Qualco3/Data/Migrations/20171014162810_5.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Qualco3.Data.Migrations
+{
+    public partial class _5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VAT",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VAT",
+                table: "AspNetUsers");
+        }
+    }
+}
