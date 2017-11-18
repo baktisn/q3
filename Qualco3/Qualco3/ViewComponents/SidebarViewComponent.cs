cc8d7073bcbaad5ac518d50b71f9170175c5837b
baktins <baknik@hotmail.com>
2017-11-18 13:54:07 +0200

    slideBar AUTHORIZATION

--- Qualco3/Qualco3/ViewComponents/SidebarViewComponent.cs
+++ Qualco3/Qualco3/ViewComponents/SidebarViewComponent.cs
@@ -11,34 +11,41 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Qualco3.ViewComponents
 {   [Authorize]
+  
     public class SidebarViewComponent : ViewComponent
     {
         public SidebarViewComponent()
         {
         }
-
+      
+        [Authorize(Roles = "Admin")]
         public IViewComponentResult Invoke(string filter)
         {
             //you can do the access rights checking here by using session, user, and/or filter parameter
             var sidebars = new List<SidebarMenu>();
-
             //if (((ClaimsPrincipal)User).GetUserProperty("AccessProfile").Contains("VES_008, Payroll"))
             //{
             //}
 
             sidebars.Add(ModuleHelper.AddHeader("MAIN NAVIGATION"));
             sidebars.Add(ModuleHelper.AddModule(ModuleHelper.Module.Home));
-            sidebars.Add(ModuleHelper.AddModule(ModuleHelper.Module.Bills));
-            sidebars.Add(ModuleHelper.AddModule(ModuleHelper.Module.Settlements));
-            sidebars.Add(ModuleHelper.AddModule(ModuleHelper.Module.Contact));
-            sidebars.Add(ModuleHelper.AddTree("Admin"));
-            sidebars.Last().TreeChild = new List<SidebarMenu>()
+            if (!User.IsInRole("Admin"))
+            {
+                sidebars.Add(ModuleHelper.AddModule(ModuleHelper.Module.Bills));
+                sidebars.Add(ModuleHelper.AddModule(ModuleHelper.Module.Contact));
+            }
+
+            if (User.IsInRole("Admin"))
+            { 
+                sidebars.Add(ModuleHelper.AddTree("Admin"));
+                sidebars.Last().TreeChild = new List<SidebarMenu>()
             {
                 ModuleHelper.AddModule(ModuleHelper.Module.ApplicationUsers),
                 ModuleHelper.AddModule(ModuleHelper.Module.GetFile),
                 ModuleHelper.AddModule(ModuleHelper.Module.PostFile),
                 ModuleHelper.AddModule(ModuleHelper.Module.Error)
             };
+            }
 
             return View(sidebars);
         }
